8e47d26184822f1eb7cb2204bdca1ff93902041d
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2022-07-17 14:05:34 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,17 +7,13 @@ import (
 )
 
 func main() {
-	// Create new Fiber instance
 	app := fiber.New()
 
-	// serve Single Page application on "/web"
-	// assume static file at dist folder
 	app.Static("/web", "dist")
 
 	app.Get("/web/*", func(ctx *fiber.Ctx) error {
 		return ctx.SendFile("./dist/index.html")
 	})
 
-	// Start server on http://localhost:3000
 	log.Fatal(app.Listen(":3000"))
 }
